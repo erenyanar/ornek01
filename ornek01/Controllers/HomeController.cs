6369d6b72f048177f59f4e92dcf5340614b1700a
YazilimI <YazilimI@DESKTOP-VT4NA0L>
2024-02-23 12:25:22 +0300

    Add project files.

---
+++ ornek01/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ornek01.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewResult result = View();
+            //return View();//
+            return result;
+        }
+
+    
+    }
+}
---
+++ ornek01/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ornek01.Controllers
+{
+
+   
+
+    public class ProductController : Controller
+    {
+        public ContentResult GetProducts()
+        {
+
+      ContentResult resuslt = Content("Selam Dünya");
+            return resuslt;
+        }
+      
+        
+    }
+}
